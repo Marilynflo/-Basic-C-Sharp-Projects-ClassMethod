89add3ca4ff0af2b23691de717bdfe4d4b5c6b4d
mmf09 <mmf09@YPC>
2023-01-04 21:53:14 -0800

    Add project files.

---
+++ ClassMethod/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace ClassMethod
+{
+    public class Class1
+    {
+        public void MathMethod1(int num1, int num2) //creating a class with void method
+        {
+            int result = num1 / 2;
+            int result2 = num2 / 2;
+            Console.WriteLine("Your number divided by 2 = " + result2);
+
+        }
+
+        public void MathMethod2(int num3, int num) //overloading method
+        {
+            int result5 = num3 * num;
+            Console.WriteLine(num3 + " x " + num + " = " + result5);
+        }
+
+        public static int MathMethod2(int num1, out int num2) //declaring class to be static
+        {
+            num2 = num1 + 18;
+            return num2;
+
+        }
+    }
+}
---
+++ ClassMethod/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: "); //asking for user input
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int num1 = 19;
+            int num3 = 50;
+            int num = 10;
+
+            Class1 FirstOp = new Class1(); //instantiating the class
+            FirstOp.MathMethod1(num1, num2);
+            FirstOp.MathMethod2(num3, num);
+
+            Console.ReadLine();
+        }
+    }
+}
